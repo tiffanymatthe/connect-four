1f9c7676c20f0e0d266784b75c53e549c953f18b
tiffanymatthe <tiffanymatthe@gmail.com>
2022-01-01 22:02:43 -0800

    rearrange code to play AlphaBeta against MCTS

--- Connect-Four_Zero/MainExecution.py
+++ Connect-Four_Zero/MainExecution.py
@@ -2,10 +2,13 @@
 from Network import Network
 from NetworkTraining import NetworkTraining
 from C4Config import C4Config
+from C4Game import C4Game
 from SharedStorage import SharedStorage
 from ReplayBuffer import ReplayBuffer
 
 from timeit import default_timer as timer
+import cProfile
+import pstats
 
 def time_game():
     network = Network()
@@ -15,9 +18,33 @@ def time_game():
     end = timer()
     print(end - start)
 
+def profile_game():
+    # https://stackoverflow.com/a/68805743
+    with cProfile.Profile() as pr:
+        time_game()
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    # Now you have two options, either print the data or save it as a file
+    # stats.print_stats() # Print The Stats
+    stats.dump_stats("game_profile.prof") # Saves the data in a file, can me used to see the data visually
+    # to view data, use pip install snakeviz, then in terminal type snakeviz game_profile.prof.
+
+def profile_inference():
+    network = Network()
+    game = C4Game()
+    image = game.make_image(-1)
+    with cProfile.Profile() as pr:
+        value, policy = network.inference(image)
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.dump_stats("inference_profile.prof")
+    print(value)
+    print(policy)
+
 def train_network():
     config = C4Config()
     NetworkTraining.alphazero(config)
 
 if __name__ == "__main__":
-    time_game()
+    # profile_inference()
+    train_network()
