2a59459b355ad6ca96d30d6c25afc6718a4ba38c
tiffanymatthe <tiffanymatthe@gmail.com>
2022-01-01 21:29:32 -0800

    refactored executing code to MainExecution and added timing for MCTS. Currently at ~28 seconds per game

--- Connect-Four_Zero/MainExecution.py
+++ Connect-Four_Zero/MainExecution.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 from Network import Network
+from NetworkTraining import NetworkTraining
+from C4Config import C4Config
+from SharedStorage import SharedStorage
+from ReplayBuffer import ReplayBuffer
+
+from timeit import default_timer as timer
+
+def time_game():
+    network = Network()
+    config = C4Config()
+    start = timer()
+    NetworkTraining.play_game(config, network)
+    end = timer()
+    print(end - start)
+
+def train_network():
+    config = C4Config()
+    NetworkTraining.alphazero(config)
 
 if __name__ == "__main__":
-    network = Network()
-    network.print_model_summary()
-    # print(network.get_weights())
+    time_game()
