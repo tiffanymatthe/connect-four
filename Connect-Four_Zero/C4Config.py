6777a643a08474d8233449a3813dc991fa8b518e
tiffanymatthe <tiffanymatthe@gmail.com>
2022-01-05 12:26:43 -0800

    modify config

--- Connect-Four_Zero/C4Config.py
+++ Connect-Four_Zero/C4Config.py
@@ -4,7 +4,7 @@ class C4Config(object):
 
     def __init__(self, model_name=None):
         # Self-Play
-        self.num_actors = 3 # 5000
+        self.num_actors = 4 # 5000
         self.num_sampling_moves = 10 # 30
         self.max_moves = 42 + 1  # 512 for chess and shogi, 722 for Go.
         self.num_simulations = 25 # 800
@@ -15,14 +15,14 @@ class C4Config(object):
         self.root_exploration_fraction = 0.25 # epsilon?
 
         # UCB formula
-        self.pb_c_base = 19652 # puct?
+        self.pb_c_base = 19652
         self.pb_c_init = 1.25
 
         # Training
         self.training_steps = int(20)
-        self.checkpoint_interval = int(2)
-        self.window_size = int(50) # int(1e6)
-        self.batch_size = 20 * 50 // 2 # 4096
+        self.checkpoint_interval = int(4)
+        self.window_size = int(10) # int(1e6)
+        self.batch_size = 20 * 10 // 4 # 4096
 
         self.weight_decay = 1e-4
         self.momentum = 0.9
