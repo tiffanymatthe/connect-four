10aaec0161b3926d814a1959395f9f243dde2b5f
ManmeetKP123 <purewalmanmeet93@gmail.com>
2022-01-05 14:20:53 -0800

    added some loss function stuff

--- Connect-Four_Zero/Residual_CNN.py
+++ Connect-Four_Zero/Residual_CNN.py
@@ -122,7 +122,29 @@ class Residual_CNN(Gen_Model):
 
         model = Model(inputs=[main_input], outputs=[vh, ph])
 
+        losses = {
+            "value_head": "mse", 
+            "policy_head":"categorical_crossentropy"
+        }
+        model.compile(optimizer='sgd', loss=losses)
+
+
         return model
     
-    def compile_model(self):
-        
+
+import tensorflow as tf
+
+def softmax_cross_entropy_with_logits(y_true, y_pred):
+
+	p = y_pred
+	pi = y_true
+
+	zero = tf.zeros(shape = tf.shape(pi), dtype=tf.float32)
+	where = tf.equal(pi, zero)
+
+	negatives = tf.fill(tf.shape(pi), -100.0) 
+	p = tf.where(where, negatives, p)
+
+	loss = tf.nn.softmax_cross_entropy_with_logits(labels = pi, logits = p)
+
+	return loss
