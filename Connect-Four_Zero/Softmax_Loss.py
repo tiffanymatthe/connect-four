a3d3dd5a34f51bbfbbc209e4b39294cec7c4d2d6
tiffanymatthe <tiffanymatthe@gmail.com>
2022-01-05 19:29:23 -0800

    debugging in softmax

--- Connect-Four_Zero/Softmax_Loss.py
+++ Connect-Four_Zero/Softmax_Loss.py
@@ -6,18 +6,22 @@ def softmax_cross_entropy_with_logits(y_true, y_pred):
     p = y_pred # probability distribution, -1 to 1?
     pi = y_true # probability distribution with values from 0 to 1
 
-    zero = tf.zeros(shape=tf.shape(pi), dtype=tf.int64)
+    print(type(p))
+    print(type(pi))
+
+    pi = tf.constant(pi, dtype=tf.float32)
+    p = tf.constant(p, dtype=tf.float32)
+
+    zero = tf.zeros(shape=tf.shape(pi), dtype=tf.float32)
     where = tf.equal(pi, zero)
-    where = tf.cast(where, tf.float32)
     negatives = tf.fill(tf.shape(pi), -100.0) 
     p = tf.where(where, negatives, p)
-    pi = tf.constant(pi, dtype=tf.float32)
 
     loss = tf.nn.softmax_cross_entropy_with_logits(labels = pi, logits = p)
 
     return loss
 
 # if __name__ == "__main__":
-#     y_true = [0,0,3,0,180,0,0]
+#     y_true = ([0,0,3,0,180,0,0]
 #     y_pred = [0.1, 0.9, 0,40,0,0,0]
 #     print(softmax_cross_entropy_with_logits(y_true, y_pred))
