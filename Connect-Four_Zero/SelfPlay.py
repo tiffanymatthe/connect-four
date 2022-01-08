e4e4f6b136b52333927daa5052879131234ca385
tiffanymatthe <tiffanymatthe@gmail.com>
2022-01-07 17:02:07 -0800

    remove buggy code in run_selfplay

--- Connect-Four_Zero/SelfPlay.py
+++ Connect-Four_Zero/SelfPlay.py
@@ -28,12 +28,14 @@ class SelfPlay():
         network = Network(config, model_name=config.model_name) # loads model from files
         id = multiprocessing.current_process()._identity[0]
         print("Starting self-play for process {}".format(id))
-        while replay_buffer.get_iteration_size() < config.num_games:
+        game_num = 0
+        games_to_play = round(config.num_games * 1.0 / config.num_actors)
+        while game_num < games_to_play:
             game = SelfPlay.play_game(config, network, rand=rand)
             if random.randint(1,4) == 1:
                 game.see_board()
             replay_buffer.save_game(game)
-            print("Game {}/{} finished by process {}".format(replay_buffer.get_iteration_size(), config.num_games, id))
+            print("Game {}/{} finished by process {}".format(game_num, games_to_play, id))
 
     # Each game is produced by starting at the initial board position, then
     # repeatedly executing a Monte Carlo Tree Search to generate moves until the end
